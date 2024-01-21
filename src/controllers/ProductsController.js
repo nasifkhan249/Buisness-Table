8b834d74f364433a70eb433446c238d578e0b8c0
Nasif Mustufa Khan <97841869+nasifkhan249@users.noreply.github.com>
2024-01-21 14:22:57 +0600

    updated

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,36 +1,27 @@
-const ProductsModel=require("../models/ProductModel");
-
+const ProductModel=require("../models/ProductModel");
 exports.ProductList=async(req,res)=>{
     try {
         let pageNo=Number(req.params.pageNo);
         let perPage=Number(req.params.perPage);
-        let searchValue=req.params.searchKey;
-        const skipRow=(pageNo-1)*perPage;
-        let Rows;
-        let Total;
+        let searchValue=req.params.searchValue;
+        let skipValue=(pageNo-1)*perPage;
+        let data;
+        // let rows;
 
-        if (searchValue!=="0") {
-            let SearchRgx = {"$regex": searchValue, "$options": "i"}
-            let SearchQuery = {$or: [{title: SearchRgx}]}
-    
-            Total = (await ProductsModel.aggregate([{$match: SearchQuery}, {$count: "total"}]))[0]['total']
-            Rows = await ProductsModel.aggregate([{$match: SearchQuery}, {$skip: skipRow}, {$limit: perPage}])
-        } else {
-            Total = (await ProductsModel.aggregate([{$count: "total"}]))[0]['total']
-            Rows = await ProductsModel.aggregate([{$skip: skipRow}, {$limit: perPage}])
+        if(searchValue!=='0'){
+            let SearchRgx={"$regex":searchValue,"$options":"i"};
+            let SearchQuery={$or:[{title:SearchRgx},{
+            category:SearchRgx},{remark:SearchRgx},
+            {brand:SearchRgx}]};
+
+            data=await ProductModel.aggregate([{$match:SearchQuery},{$count:"total"}]);
+            rows=await ProductModel.aggregate([{$match:SearchQuery},{$skip:skipValue},{$limit:perPage}])
+
+        }else{
+            data=await ProductModel.aggregate([{$count:"total"}]);
+            rows=await ProductModel.aggregate([{$skip:skipValue},{$limit:perPage}])
         }
-        res.status(200).json({status: "success", total: Total, data: Rows})
-    } catch (error) {
-        res.status(200).json({status:"fail",error:console.log(error)});
-    }
-}
-
-
-exports.Product=async(req,res)=>{
-    try {
-        let reqBody=req.body;
-        let data=await ProductsModel.create(reqBody);
-        res.status(200).json({status: "success", data})
+        res.status(200).json({status: "success", total: data, data: rows});
     } catch (error) {
         res.status(200).json({status:"fail",error:console.log(error)});
     }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,7 @@ const express=require('express');
 const ProductController=require('../controllers/ProductsController');
 
 const router=express.Router();
-router.post("/Product",ProductController.Product);
-router.get("/ProductList/:pageNo/:perPage/:searchKey",ProductController.ProductList);
+
+router.get("/ProductList/:pageNo/:perPage/:searchValue",ProductController.ProductList);
 
 module.exports=router;
